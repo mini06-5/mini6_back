964a57ff0cf23fd102635e7f408be4fe594a5911
sohanmin <world2013@naver.com>
2026-06-23 17:54:47 +0900

    deploy.sh modification

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step1
+# 프로젝트 이름 대신 jar 파일명을 직접 찾도록 변경
+JAR_NAME=$(ls -tr $REPOSITORY/build/libs/*-SNAPSHOT.jar | grep -v plain | tail -n 1)
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+# jar 파일명으로 실행 중인 프로세스를 찾습니다.
+CURRENT_PID=$(pgrep -f $JAR_NAME)
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+echo "> JAR Name: $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+nohup java -jar $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
